1fd7b050fb4a94f710d6e7d9b22bce340df028e6
Souhayl-s <souhaylsejjari1@gmail.com>
2025-01-09 17:09:06 -0500

    change vite config

--- site-web/vite.config.js
+++ site-web/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    open: true,
-    port: 3000
-  },
+  base: '/Souhayl-s/sport-reservation-app', // Replace <your-repo-name> with the name of your GitHub repository
   plugins: [react()],
 });
