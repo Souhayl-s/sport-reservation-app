b2fa2f57336efd5eb9767ed57c0a017a0560e189
Souhayl-s <souhaylsejjari1@gmail.com>
2025-01-09 20:53:13 -0500

    smth

--- site-web/vite.config.js
+++ site-web/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/sport-reservation-app"
+  base: "/sport-reservation-app/"
 });
 
