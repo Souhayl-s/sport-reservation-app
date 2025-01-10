42e3ab0bbac3f40ce97cada6de1ae53121eac299
Souhayl-s <souhaylsejjari1@gmail.com>
2025-01-09 20:40:34 -0500

    smth

--- site-web/src/App.jsx
+++ site-web/src/App.jsx
@@ -8,7 +8,7 @@ import Header from "./components/header";
 
 function App() {
   return (
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <Router basename="/sport-reservation-app" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ReservationProvider>
         <Header/>
         <Routes>
