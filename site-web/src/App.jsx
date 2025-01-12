2395eda7b2dce190164dbbec3a3111008eb2f6b8
Souhayl-s <souhaylsejjari1@gmail.com>
2025-01-12 14:27:02 -0500

    correct typo

--- site-web/src/App.jsx
+++ site-web/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <ReservationProvider>
         <Header/>
         <Routes  >
-          <Route path="/sport-reservation-app" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/plateau/:plateauId" element={<PlateauPage />} />
           <Route path="/reservation" element={<ReservationPage />} />
           <Route
